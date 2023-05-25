a9bfeb64a6e2dabd9324733620c38f5c0371e610
here_io <52244687+iopark@users.noreply.github.com>
2023-05-25 17:31:23 +0900

    feat: Managers script add

---
+++ Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float speed = 3f; // Speed at which the object moves
+    public Transform endpoint;
+
+    public enum State { Idle, Launch };
+    public State curState;
+
+    private void Start()
+    {
+        endpoint = GetComponent<Transform>();
+        curState = State.Idle;
+        //이벤트 구문 추가 
+        GameManager.Pool.Trigger += SwithState;
+    }
+    private void Update()
+    {
+        // Translate the object to the left
+        switch (curState)
+        {
+            case State.Idle:
+                IdleState();
+                break;
+            case State.Launch:
+                LaunchState();
+                break;
+        };
+    }
+
+    private void SwithState()
+    {
+        curState = State.Launch;
+    }
+
+    private void IdleState()
+    {
+        //대기만 한다. 
+        return;
+    }
+
+    private void LaunchState()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+
+        // Check if the object reaches the endpoint
+        if (transform.position.x <= endpoint.position.x)
+        {
+            GameManager.Pool.ReturnBlockToPool(gameObject);
+        }
+    }
+}
